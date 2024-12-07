9e58dabf9be37e2a9e9943a8bae7685244bf838e
Shivam <sharmashivam1909@gmail.com>
2024-12-07 16:10:14 +0530

    Refactor code structure.

---
+++ Secret-Santa-UI/src/context/AlertContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const AlertContext = createContext();
+
+export const useAlert = () => {
+  return useContext(AlertContext);
+};
+
+export const AlertProvider = ({ children }) => {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type = 'success') => {
+    setAlert({ message, type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 5001);
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert, showAlert }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
